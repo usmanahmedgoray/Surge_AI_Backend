f3accc3cb0983ba1afa455f4dd50224a27486235
usmanahmedgoray <usmanahmedkharal@gmail.com>
2023-09-30 20:41:59 +0500

    First Commit

---
+++ index.js
@@ -0,0 +1,39 @@
+import express from "express";
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+
+import connectDB from "./mongodb/connect.js";
+import postRoutes from './routes/postRoutes.js';
+import dalleRoutes from './routes/dalleRoutes.js'
+dotenv.config();
+const port = 8080;
+
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: '50mb' }));
+
+app.use('/api/v1/post',postRoutes)
+app.use('/api/v1/dalle',dalleRoutes)
+
+app.get('/', async (req, res) => {
+    res.send('Hello')
+})
+
+const startServer = async () => {
+    try {
+        connectDB('mongodb+srv://usmanahmedgoray:Goraya61chak@cluster0.zghfzmw.mongodb.net/test?retryWrites=true&w=majority')
+        
+        app.listen(port, () => {
+            console.log(`Server has started on port "http://localhost:${port}"`)
+        })
+    } catch (error) {
+        console.log(error)
+    }
+
+
+}
+
+startServer();
+
+// Export the Express API
+module.exports = app
---
+++ routes/dalleRoutes.js
@@ -0,0 +1,106 @@
+// Impoting the required libraries
+import express from "express";
+import * as dotenv from 'dotenv';
+import { Configuration, OpenAIApi } from "openai";
+import multer from "multer";
+import fs from "fs"
+import sharp from "sharp";
+
+// generate random Id 
+let id = "id" + Math.random().toString(16).slice(2);
+// Declare the global file  path
+let filePath;
+
+// setup the storage Information
+
+const storage = multer.diskStorage({
+    destination: (req, file, next) => {
+        next(null, "../server/public")
+    },
+    filename: (req, file, next) => {
+        // console.log("file", file)
+        next(null, Date.now() + "-" + file.originalname)
+    }
+})
+
+// upload the file to the local-storage
+
+const upload = multer({ storage: storage }).single("file")
+
+dotenv.config();
+const router = express.Router();
+
+// Configuration of Open AI API
+
+const configuration = new Configuration({
+    // apiKey: "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"
+    // apiKey: "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"
+    apiKey: "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"
+
+});
+
+const openai = new OpenAIApi(configuration);
+
+//  Route 01 => Image Generating through Command prompt => method :> POST => localhost:8080/api/v1/dalle
+router.route('/').post(async (req, res) => {
+
+    try {
+        // destructuring the request body
+        const { prompt } = req.body;
+
+        const aiResponse = await openai.createImage({
+            prompt,
+            n: 1,
+            size: "1024x1024",
+            response_format: "b64_json"
+        });
+        // res.status(200).json(aiResponse)
+        const image = aiResponse.data.data[0].b64_json;
+        res.status(200).json({ photo: image })
+    } catch (error) {
+        res.json(error);
+        res.status(500).send(error?.response.data.error.massage)
+    }
+})
+
+//  Route 02 => Image Varation Generating => method :> POST => localhost:8080/api/v1/dalle/variation
+
+router.route("/variation").post(async (req, res) => {
+    upload(req, res, function (err) {
+        if (err instanceof multer.MulterError) {
+            // A Multer error occurred when uploading.
+        } else if (err) {
+            // An unknown error occurred when uploading.
+        }
+
+        // Store the path in the filePath variable
+        filePath = req.file.path
+        // setup the sharp library for resize the Image for API request
+        let inputImage = filePath;
+        let outputImage = `../server/cropImage/${id}.png`
+        sharp(inputImage)
+            .resize({ width: 512, height: 512 })
+            .toFile(outputImage)
+            .then(async() => {await generateImage(); fs.unlinkSync(filePath)})
+            .catch(err => console.log(err, "Error occured"))
+
+    })
+
+    // function for requesting the Open AI API and  generate the Variation of the Image
+
+    const generateImage = async () => {
+        const response = await openai.createImageVariation(
+            fs.createReadStream(`../server/cropImage/${id}.png`),
+            1,
+            "1024x1024",
+            "b64_json"
+        );
+
+        const image = response.data.data[0].b64_json
+        fs.unlinkSync(`../server/cropImage/${id}.png`)
+        res.status(200).json(image)
+    }
+})
+
+
+export default router;
